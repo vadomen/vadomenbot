113021c25ea67b2cec6876a2727f5aeaa5cc662c
Vadim Pospelov <vadomen@gmail.com>
2022-03-15 15:39:46 +0200

    Timeout adjustments

--- src/feeder.ts
+++ src/feeder.ts
@@ -4,14 +4,14 @@ const feeder = new RssFeedEmitter();
 const launch = (telegram: any, chatName: String) => {
   feeder.add({
     url: 'https://www.liga.net/news/top/rss.xml',
-    refresh: 5000,
+    refresh: 10 * 60 * 1000, // 10 minutes
     eventName: 'liga'
   });
 
   feeder.on('liga', function(item: any) {
     const message = `${item.title}
   ${item.link}`;
-    telegram.sendMessage(chatName, message);
+    setTimeout(() => telegram.sendMessage(chatName, message), 500);
   });
 };
 
