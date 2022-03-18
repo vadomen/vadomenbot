40bc909bc54b82d75fea7c4a33f2fe8b027d533e
Vadim Pospelov <vadomen@gmail.com>
2022-03-18 17:03:04 +0200

    Limits configuration

--- src/feeder.ts
+++ src/feeder.ts
@@ -5,7 +5,7 @@ import moment from 'moment';
 const parser: Parser = new Parser({});
 
 export class Feeder {
-  interval: number = Number(process.env.FEED_REFRESH_INTERVAL) || 60 as number;
+  interval: number = Number(process.env.FEED_REFRESH_INTERVAL) || 30 as number; // in minutes
   telegram: Telegram;
   channelId: string | number;
   sources: string[];
@@ -13,31 +13,28 @@ export class Feeder {
   constructor(telegram: Telegram, channelId: string | number) {
     this.telegram = telegram;
     this.channelId = channelId;
-    this.interval = this.interval * 60 * 1000; // 1 hour
-    this.sources = ['https://censor.net/includes/news_uk.xml', 'https://fakty.ua/rss_feed/ukraine' ,'https://www.liga.net/news/top/rss.xml'];
+    this.interval = this.interval * 60 * 1000;
+    this.sources = ['https://censor.net/includes/news_uk.xml' ,'https://www.liga.net/news/top/rss.xml'];
   }
 
   broadcast = async () => {
     try {
+      console.info('Process started');
       const sourcesPromises = this.sources.map(s => parser.parseURL(s));
       let feeds = await Promise.allSettled(sourcesPromises);
       feeds = feeds.filter(f => f.status === 'fulfilled');
-      const news = feeds.reduce((previousValue: any[], currentValue: any) => ([...previousValue, ...currentValue.value.items]),[]);
+      let news = feeds.reduce((previousValue: any[], currentValue: any) => ([...previousValue, ...currentValue.value.items]),[]);
+      news = news.slice(0, 29);
       const date = moment().subtract(this.interval, 'milliseconds');
-      let counter = 0;
       news.forEach(item => {
         if(moment(item.pubDate).isAfter(date)) {
-          const message = `${item.link} ${item.pubDate} `;
-          if(counter < 29) {
-            this.telegram.sendMessage(this.channelId, message)
-          }
-          counter++;
+          const message = `${item.link} ${item.pubDate}`;
+          this.telegram.sendMessage(this.channelId, message);
         }
       });
     } catch (e: any) {
       throw new Error(e);
     }
-
   }
   launch = () => {
     this.broadcast();
